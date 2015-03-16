2315d36e6d075a7db735b40fb2a23824777c88cd
Andy Chu <andychu@google.com>
2015-03-16 15:13:24 -0700

    Got it working

--- tests/rappor_regtest.py
+++ tests/rappor_regtest.py
@@ -6,22 +6,27 @@ rappor_regtest.py
 import sys
 
 # For gen_sim_input.py
-INPUT_PARAMS = [
-]
+INPUT_PARAMS = {
+    'e1': ('exp', 10000, 100),
+    'e2': ('exp', 100000, 100),
+    }
 
 # For rappor_sim.py
-RAPPOR_PARAMS = [
-]
+RAPPOR_PARAMS = {
+    # k,h,m,p,q,f
+    'r1': (16, 2, 64, 0.5, 0.75, 0.5),
+    }
 
 # Pairs of (input params, rappor params)
 
 # should we have a name?
 # or just identify by T1 .. Tn
 
-
-
-TEST_CASES = [
-]
+TEST_CASES = {
+    # same parameters as the demo
+    'demo': ('e1', 'r1'),
+    'chrome': ('e2', 'r1'),
+    }
 
 
 def main(argv):
@@ -51,9 +56,18 @@ def main(argv):
   # - Reuse the same sim input
   # - Reuse the same map file - rappor library can cache it
 
-  print r'10 10 10'
-  print r'20 20 20'
+  rows = []
+  for test_case, (input_name, rappor_name) in TEST_CASES.iteritems():
+    input_params = INPUT_PARAMS[input_name]
+    rappor_params = RAPPOR_PARAMS[rappor_name]
+    #print input_params, rappor_params
+    row = tuple([test_case,]) + input_params + rappor_params
+    rows.append(row)
 
+  for row in rows:
+    for cell in row:
+      print cell,  # print it with a space after it
+    print  # new line after row
 
 
 if __name__ == '__main__':
