b3e12ece4bcd3dded493273957fb5ecdd1cff0b5
Andy C <andy@chubot.org>
2015-03-16 23:36:37 -0700

    Parsing it

--- tests/make_summary.py
+++ tests/make_summary.py
@@ -9,10 +9,24 @@ import sys
 
 def main(argv):
   base_dir = argv[1]
+
+  # This file has the test case names, in the order that they should be
+  # displayed.
   path = os.path.join(base_dir, 'test-cases.txt')
   with open(path) as f:
-    for line in f:
-      print line
+    test_cases = [line.strip() for line in f]
+
+  for case in test_cases:
+    spec = os.path.join(base_dir, case, 'spec.txt')
+    with open(spec) as s:
+      spec_row = s.readline().split()
+
+    print spec_row
+
+    metrics = os.path.join(base_dir, case + '_report', 'metrics.csv')
+    with open(metrics) as m:
+      print m.read()
+
 
 
 if __name__ == '__main__':
